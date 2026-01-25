35499110c5202da8be3692ba2f06deb604b4c611
zurayq <bdalwahdzryq0@gmail.com>
2026-01-25 22:16:02 +0300

    Fix i18n: Load actual translation messages instead of empty object

--- i18n/request.js
+++ i18n/request.js
@@ -1,2 +1,14 @@
 import { getRequestConfig } from 'next-intl/server';
-export default getRequestConfig(async () => ({ locale: 'en', messages: {} }));
+import { notFound } from 'next/navigation';
+
+const locales = ['en', 'ar', 'tr', 'it'];
+
+export default getRequestConfig(async ({ locale }) => {
+    if (!locales.includes(locale)) {
+        notFound();
+    }
+
+    return {
+        messages: (await import(`../messages/${locale}.json`)).default
+    };
+});
