9cd516c07c7c5c65dd379241281aefa690358170
zurayq <bdalwahdzryq0@gmail.com>
2026-01-25 23:30:00 +0300

    Explicitly configure next-intl to use i18n.ts

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const withNextIntl = createNextIntlPlugin();
+const withNextIntl = createNextIntlPlugin('./i18n.ts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
